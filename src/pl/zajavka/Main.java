0f9e51086213e608640417e5e6ded2ecd1402569
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-11-07 12:03:57 +0100

    initial commit

--- src/pl/zajavka/Main.java
+++ src/pl/zajavka/Main.java
@@ -2,6 +2,12 @@ package pl.zajavka;
 
 public class Main {
     public static void main(String[] args) {
-
+        // VCS - Version Control System
+        // Lokalne systemy kontroli wersji (RCS)
+        // = -SPOF - single point of failure
+        // Scentralizowane systemy kontroli wersji (CVCS - Centralized Version Control System)
+        // Rozproszone systemy kontroli wersji (DVCS - Distributed Version Control system)
+        // - Mercurial
+        // - Git
     }
 }
