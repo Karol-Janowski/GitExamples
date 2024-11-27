5189900209ccd8f10e3aa1d0d8c9e32bbf4980b6
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-11-27 12:45:53 +0100

    added git notes

--- src/pl/zajavka/Main.java
+++ src/pl/zajavka/Main.java
@@ -3,5 +3,11 @@ package pl.zajavka;
 public class Main {
     public static void main(String[] args) {
         System.out.println("dupa");
+
+        // git revert
+        // git reset
+        // -soft
+        // -mixed
+        // -hard
     }
 }
